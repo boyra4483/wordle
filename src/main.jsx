6804ceba11fc5f1da6058bea36391432d60bba42
boyra <b9892213@gmail.com>
2025-03-09 23:59:20 +0300

    i removed the hash router from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -6,8 +6,6 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>
 );
